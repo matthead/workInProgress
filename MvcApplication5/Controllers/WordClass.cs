506ec45f6cca8465f5a2a0fbcd7c6c670b325afc
Matt <msb1320@gmail.com>
2014-10-11 21:49:08 -0400

    added ajax,  incorrect letters, won logic

--- MvcApplication5/Controllers/WordClass.cs
+++ MvcApplication5/Controllers/WordClass.cs
@@ -7,14 +7,10 @@ namespace MvcApplication5.Controllers
 {
     public class WordClass
     {
-        public WordClass()
-        {
-            count = 0;
-        }
         public static string GetWord()
         {
-            count = 0;
-            /*string url = "http://randomword.setgetgo.com/get.php";
+           /* count = 0;
+            string url = "http://randomword.setgetgo.com/get.php";
             var request = System.Net.WebRequest.Create(url);
             request.ContentType = "application/json; charset=utf-8";
             var response = (System.Net.HttpWebResponse)request.GetResponse();
@@ -24,11 +20,9 @@ namespace MvcApplication5.Controllers
             }
             return word;*/
             word = "matt";
-            count++;
             return word;
             
         }
         public static string word { get; private set; }
-        public static int count;
     }
 }
